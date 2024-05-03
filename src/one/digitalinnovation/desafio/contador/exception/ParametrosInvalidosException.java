a5dacbc3e799a113b91f783db065f6ef37468382
AnGabSS <angelo.gs.santana@outlook.com>
2024-05-03 20:25:33 -0300

    Challenge completed

---
+++ src/one/digitalinnovation/desafio/contador/exception/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.desafio.contador.exception;
+
+public class ParametrosInvalidosException extends Exception{
+
+    private static final long serialVersionUID = 1L;
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
